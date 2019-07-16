e7a8a6c9dcd821d9d0e46a73b16f15ad2a5532d1
wangjinxi <wangjinxi@che001.com>
2019-07-16 13:44:40 +0800

    update

--- src/DubboParser.php
+++ src/DubboParser.php
@@ -12,44 +12,12 @@ class DubboParser
     {
         $decoder = new Decoder;
         $decoder->feed($this->removeHeaderForDubbo($data));
-        $obj = $decoder->finalize();
-        return $this->recursive($obj);
+        return $decoder->finalize();
     }
 
     private function removeHeaderForDubbo($data)
     {
         return substr($data, 17);
-    }
-
-    private function recursive($data)
-    {
-        if ($data instanceof Vector)
-        {
-            return $this->recursive($data->elements());
-        }
-
-        if ($data instanceof DateTime)
-        {
-            return date('Y-m-d H:i:s', $data->unixTime());
-        }
-
-        if ($data instanceof stdClass)
-        {
-            foreach ($data as $key => $value)
-            {
-                $data->$key = $this->recursive($value);
-            }
-        }
-
-        if (is_array($data))
-        {
-            foreach ($data as $key => $value)
-            {
-                $data[$key] = $this->recursive($value);
-            }
-        }
-
-        return $data;
     }
 
 }
